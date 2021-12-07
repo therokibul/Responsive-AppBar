498643c49e8043d0b9f7ae3465515bd3a67a444c
Rokibul Hasan <therokibul@gmail.com>
2021-12-07 11:41:39 +0600

    done

--- lib/responsive_appbar.dart
+++ lib/responsive_appbar.dart
@@ -4,21 +4,20 @@ class ResponsiveAppBar extends StatelessWidget implements PreferredSizeWidget {
   const ResponsiveAppBar({Key? key}) : super(key: key);
 
   @override
-  // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(50.0);
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     if (screenWidth < 600) {
       return AppBar(
-        backgroundColor: Color(0xff2C2F30),
-        leading: Icon(Icons.menu_outlined),
-        title: Text(
+        backgroundColor: const Color(0xff2C2F30),
+        leading: const Icon(Icons.menu_outlined),
+        title: const Text(
           'Responsive AppBar',
           style: TextStyle(color: Color(0xff21B2BB)),
         ),
         actions: [
-          Icon(
+          const Icon(
             Icons.search_outlined,
           ),
           SizedBox(
@@ -28,14 +27,45 @@ class ResponsiveAppBar extends StatelessWidget implements PreferredSizeWidget {
       );
     } else {
       return AppBar(
-        backgroundColor: Color(0xff2C2F30),
-        leading: Icon(Icons.menu_outlined),
-        title: Text(
+        backgroundColor: const Color(0xff2C2F30),
+        leading: const Icon(Icons.menu_outlined),
+        title: const Text(
           'Responsive AppBar',
-          style: TextStyle(color: Color(0xff21B2BB)),
+          style: TextStyle(
+              color: Color(
+            0xff21B2BB,
+          )),
         ),
         actions: [
-          Icon(
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Home',
+              style: TextStyle(fontSize: 18, color: Color(0xff21B2BB)),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'About Us',
+              style: TextStyle(fontSize: 18, color: Color(0xffFFFFFF)),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Blog',
+              style: TextStyle(fontSize: 18, color: Color(0xffFFFFFF)),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Contact',
+              style: TextStyle(fontSize: 18, color: Color(0xffFFFFFF)),
+            ),
+          ),
+          const Icon(
             Icons.search_outlined,
           ),
           SizedBox(
